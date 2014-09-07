b4715b94e88c166cea709a4a32074fd9082e7090
Florian Wittmann <dev@florianwittmann.de>
2014-09-07 15:42:09 +0200

    complete new version 0.3.0

--- qml/difficulties.js
+++
@@ -1,7 +0,0 @@
-var difficulties = {
-    0 : qsTr("Very easy"),
-    1 : qsTr("Easy"),
-    2 : qsTr("Normal"),
-    3 : qsTr("Difficult"),
-    4 : qsTr("Hard")
- };
--- qml/gamemodes.js
+++
@@ -1,5 +0,0 @@
-var gamemodes = {
-    q30 : qsTr("30 questions"),
-    mis3 : qsTr("3 mistakes"),
-    sec60 : qsTr("60 seconds")
- };
