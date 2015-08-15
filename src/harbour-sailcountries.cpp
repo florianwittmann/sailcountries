f331bef07b83daa6c078c9756d8d0fe882e18c52
Florian Wittmann <dev@florianwittmann.de>
2015-08-15 09:00:36 +0200

    Version bump to 0.4.1

--- src/harbour-sailcountries.cpp
+++ src/harbour-sailcountries.cpp
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
     view->setTitle("SailCountries");
 
 
-    view->rootContext()->setContextProperty("versionString", "0.4.0");
+    view->rootContext()->setContextProperty("versionString", "0.4.1");
 
 
     QUrl pageSource = SailfishApp::pathTo("qml/harbour-sailcountries.qml");
